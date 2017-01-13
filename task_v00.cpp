661a1a0d0bef7668bb91ed6b507220ed5e34675f
Vityalimbaev <vityalimbaev@gmail.com>
2017-01-14 00:48:49 +0400

    version 0

---
+++ task_v00.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+class Base
+{
+public:
+Base() { cout << "Construct Base object\n"; }
+~Base() { cout << "Destruct Base object\n"; }
+};
+int main(int argc, char *argv[])
+{
+cout << "* Create Object base" << endl;
+Base base;
+return 0;
+}
