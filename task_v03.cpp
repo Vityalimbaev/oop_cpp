a562e2f31a4516e2dee986b0d7cd643a67408234
Vityalimbaev <vityalimbaev@gmail.com>
2017-01-14 10:57:11 +0400

    version 0.3

---
+++ task_v03.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class Base
+{
+public:
+Base() { cout << "Construct Base object\n"; }
+~Base() { cout << "Destruct Base object\n"; }
+void func() { cout << "Function func() of class Base\n"; }
+};
+class Child: public Base
+{
+public:
+Child() { cout << "Construct Child object\n"; }
+~Child() { cout << "Destruct Child object\n"; }
+void func(){cout<<"function func() of class Child\n"; }
+};
+int main(int argc, char *argv[])
+{
+cout << "* Create Object base" << endl;
+Base base;
+cout << "* Create Object child" << endl;
+Child child;
+cout << "* Call method func() for object base" << endl;
+base.func();
+cout << "* Call method func() for object child" << endl;
+child.func();
+return 0;
+}
